6b2ebf9842a52e52558cc473bf6aa7bf4ffa9a73
hsbauer <bauer.scott@mayo.edu>
2022-08-31 15:26:45 -0500

    v2.0.0. Commiting dev configuration values

--- src/main/java/edu/mayo/cts2/framework/LexevsServiceApp.java
+++ src/main/java/edu/mayo/cts2/framework/LexevsServiceApp.java
@@ -3,13 +3,14 @@ package edu.mayo.cts2.framework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ImportResource("classpath:spring/root-context.xml")
-public class LexevsServiceApp {
+public class LexevsServiceApp extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(LexevsServiceApp.class, args);
     }
