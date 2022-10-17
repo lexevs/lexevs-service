8d80171594cbdc1125e9d749e4f8fb7ea4cf354f
hsbauer <bauer.scott@mayo.edu>
2022-10-17 14:16:06 -0500

    Removing most references to URI Resolver and function

--- src/main/java/edu/mayo/cts2/framework/service/lexevs/service/entity/EntityUriResolver.java
+++ src/main/java/edu/mayo/cts2/framework/service/lexevs/service/entity/EntityUriResolver.java
@@ -9,15 +9,12 @@
 package edu.mayo.cts2.framework.service.lexevs.service.entity;
 
 import edu.mayo.cts2.framework.model.core.ScopedEntityName;
-import edu.mayo.cts2.framework.service.lexevs.uri.UriResolver;
-import edu.mayo.cts2.framework.service.lexevs.uri.UriResolver.IdType;
 import edu.mayo.cts2.framework.service.lexevs.uri.UriUtils;
 import java.util.Arrays;
 import javax.annotation.Resource;
 import org.LexGrid.LexBIG.Exceptions.LBException;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.LexGrid.codingSchemes.CodingScheme;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,9 +22,7 @@ public class EntityUriResolver {
 		
 	@Resource
 	private LexBIGService lexBigService;
-	
-	@Resource
-	private UriResolver uriResolver;
+
 	
 	public ScopedEntityName resolveUri(String entityUri){
 		String namePart = UriUtils.getLocalPart(entityUri);
@@ -38,21 +33,16 @@ public class EntityUriResolver {
 		name.setName(namePart);
 		
 		for(String ns : Arrays.asList(namespacePart, namespacePartWithSeparator)){
-			String namespace = this.uriResolver.idToName(ns, IdType.CODE_SYSTEM);
+
 		
-			if(StringUtils.isNotBlank(namespace)){
-				name.setNamespace(namespace);
-				
-				return name;
-			} else {
+
 				try {
 					CodingScheme cs = this.lexBigService.resolveCodingScheme(ns, null);
 					name.setNamespace(cs.getCodingSchemeName());
 					return name;
 				} catch (LBException e) {
-					//didn't find it here...
-				}	
-			}
+					throw new RuntimeException("No canonical URI found here", e);
+				}
 		}
 		
 		return null;
